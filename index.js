b8970db0620397dc950baa050cfd1b093ae65336
Austin Adams <austin@farmingtonwebdesign.net>
2016-06-07 03:06:26 -0700

    Uses better LD algorithm

--- index.js
+++ index.js
@@ -115,94 +115,7 @@
  *
  */
 const R = require('ramda');
-const LD = function damerauLevenshteinDistance(source, target) {
-  if (!source || source.length === 0)
-    if (!target || target.length === 0)
-      return 0;
-    else
-      return target.length;
-  else if (!target)
-    return source.length;
-
-  var sourceLength = source.length;
-  var targetLength = target.length;
-  var score = [];
-
-  var INF = sourceLength + targetLength;
-  score[0] = [INF];
-  for (var i = 0; i <= sourceLength; i++) {
-    score[i + 1] = [];
-    score[i + 1][1] = i;
-    score[i + 1][0] = INF;
-  }
-  for (var i = 0; i <= targetLength; i++) {
-    score[1][i + 1] = i;
-    score[0][i + 1] = INF;
-  }
-
-  var sd = {};
-  var combinedStrings = source + target;
-  var combinedStringsLength = combinedStrings.length;
-  for (var i = 0; i < combinedStringsLength; i++) {
-    var letter = combinedStrings[i];
-    if (!sd.hasOwnProperty(letter))
-      sd[letter] = 0;
-  }
-
-  for (var i = 1; i <= sourceLength; i++) {
-    var DB = 0;
-    for (var j = 1; j <= targetLength; j++) {
-      var i1 = sd[target[j - 1]];
-      var j1 = DB;
-
-      if (source[i - 1] == target[j - 1]) {
-        score[i + 1][j + 1] = score[i][j];
-        DB = j;
-      }
-      else
-        score[i + 1][j + 1] = Math.min(score[i][j], Math.min(score[i + 1][j], score[i][j + 1])) + 1;
-
-      score[i + 1][j + 1] = Math.min(score[i + 1][j + 1], score[i1][j1] + (i - i1 - 1) + 1 + (j - j1 - 1));
-    }
-    sd[source[i - 1]] = i;
-  }
-  return score[sourceLength + 1][targetLength + 1];
-};
-
-// Compute the edit distance between the two given strings
-function getEditDistance(a, b) {
-  if (a.length === 0) return b.length;
-  if (b.length === 0) return a.length;
-
-  var matrix = [];
-
-  // increment along the first column of each row
-  var i;
-  for (i = 0; i <= b.length; i++) {
-    matrix[i] = [i];
-  }
-
-  // increment each column in the first row
-  var j;
-  for (j = 0; j <= a.length; j++) {
-    matrix[0][j] = j;
-  }
-
-  // Fill in the rest of the matrix
-  for (i = 1; i <= b.length; i++) {
-    for (j = 1; j <= a.length; j++) {
-      if (b.charAt(i - 1) == a.charAt(j - 1)) {
-        matrix[i][j] = matrix[i - 1][j - 1];
-      } else {
-        matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, // substitution
-          Math.min(matrix[i][j - 1] + 1, // insertion
-            matrix[i - 1][j] + 1)); // deletion
-      }
-    }
-  }
-
-  return matrix[b.length][a.length];
-};
+const LD = require('damerau-levenshtein');
 //O(n) where n = length of knownNames
 function name_match(knownNames, name) {
   let incomingNameParts = name.split(' ');
@@ -242,13 +155,17 @@ function name_match(knownNames, name) {
         else return false;
       }
 
-      let distanceArr = incomingNameParts.map((word, index) => LD(word, knownNameParts[index]));
-      let distanceTotal = R.reduce(R.add, 0, distanceArr);
+      if (incomingNameParts.length === knownNameParts.length) {
+        let distanceArr = incomingNameParts.map((word, index) => {
+          let {steps} = LD(word, knownNameParts[index]);
+          return steps;
+        });
+        let distanceTotal = R.reduce(R.add, 0, distanceArr);
+        if (distanceTotal <= incomingNameParts.length &&
+          distanceArr.indexOf(2) === -1) return true;
+      }
 
-      if(distanceTotal <= incomingNameParts.length &&
-      distanceArr.indexOf(2) === -1) return true;
-
-      if(intersection.length < 2) return false;
+      if (intersection.length < 2) return false;
     }, knownNames)
   );
   // console.log(matcheResults);
