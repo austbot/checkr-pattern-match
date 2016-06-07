dfedd4bbad7e0d1606e8a1b8820e3189a809e636
Austin Adams <austin@farmingtonwebdesign.net>
2016-06-07 05:24:04 -0700

    Adds concurrent processing of comparisons

--- index.js
+++ index.js
@@ -114,140 +114,186 @@
  *       decisions made by Checkr’s client.
  *
  */
+const cp = require('child_process');
+const Rx = require('@reactivex/rxjs');
 const R = require('ramda');
-const LD = require('damerau-levenshtein');
-//O(n) where n = length of knownNames
-function name_match(knownNames, name) {
-  let incomingNameParts = name.split(' ');
-  //Reduce the matches to a true or false
-  return R.reduce((match, value) => {
-      return value === true ? true : match;
-    }, false,
-    //Map over known names and run comparisons return match status back into a new arr
-    R.map((knownName, index) => {
-      //console.log("KNOWN", knownName, "INCOMING", name);
-      //Exact match
-      if (knownName === name) return true;
-      //Split strings into word parts
-      let knownNameParts = knownName.split(' ');
-      //Find common words
-      let intersection = R.intersection(incomingNameParts, knownNameParts);
-      //If we have a common length between the intersection and the incoming words
-      if (knownNameParts.length === intersection.length
-        || incomingNameParts.length === intersection.length) return true;
-      //If we know we have three parts and the intersection is missing one part
-      if (knownNameParts.length == 3 && incomingNameParts.length === 3 && intersection.length === 2) {
-        //Compare the first letter of the middle part
-        if (R.head(knownNameParts[1]) === R.head(incomingNameParts[1])) return true;
-      }
-      //Find the difference between the two word arrays so we can look in more detail at why we aren't finding a middle name match
-      let difference = R.difference(incomingNameParts, knownNameParts);
-      //We know two words match but we have a difference
-      if (intersection.length == 2 && difference.length > 0) {
-        let matcher = false;
-        //For all the known name parts
-        knownNameParts.forEach((value) => {
-          //Check to see if another part of the name matches the first char of the difference.
-          if (R.head(value) === R.head(difference[0])) matcher = true;
-        });
-        //Return if the matcher is true, other wise we need to consider this Not a match.
-        if (matcher) return true;
-        else return false;
-      }
+const DEBUG = true;
 
-      if (incomingNameParts.length === knownNameParts.length) {
-        let distanceArr = incomingNameParts.map((word, index) => {
-          let {steps} = LD(word, knownNameParts[index]);
-          return steps;
-        });
-        let distanceTotal = R.reduce(R.add, 0, distanceArr);
-        if (distanceTotal <= incomingNameParts.length &&
-          distanceArr.indexOf(2) === -1) return true;
-      }
-
-      if (intersection.length < 2) return false;
-    }, knownNames)
-  );
-  // console.log(matcheResults);
+function log() {
+  if (DEBUG) {
+    console.log(...arguments);
+  }
+  else return () => {
+  };
+}
+/**
+ * Helper method to create a Pure Process Fn
+ * @param appPath: string
+ * @param output: fn
+ */
+function runApp(appPath, output) {
+  //Create the process
+  let proc = cp.fork(appPath);
+  //Log the process pid
+  log(`New ${appPath} Process started: ${proc.pid}`);
+  //Setup the callback (output function)
+  proc.on('message', output);
+  //Log the exit|death 
+  proc.on('exit', function () {
+    log(`Process with pid ${proc.pid} has ended`);
+  });
+  //Return the process object for more fun
+  return proc;
 }
 
+//O(n) where n = length of knownNames
+function name_match(knownNames, name, callback) {
+  const comps = ['exact', 'arrayMatch', 'ldDistance'];
+  //Reduce the matches to a true or false
+  const matchReducer = R.reduce((match, value) => value === true ? true : match, false);
+  const matchSubject = new Rx.Subject();
+  //Map over known names and run comparisons return match status back into a new arr
+  let sent = 0, returned = 0;
+  comps.forEach((compApp, index) => {
+    let proc = runApp(`./comparisons/${compApp}`, (msg) => {
+      returned += 1;
+      matchSubject.next(Rx.Observable.of(msg));
+      log(compApp, knownNames, name, msg);
+      if (sent === returned) {
+        matchSubject.complete();
+      }
+    });
+    proc.send({names: knownNames, name});
+    sent += 1;
+  });
 
-function test() {
-  var known_names = ["Alphonse Gabriel Capone", "Al Capone"];
-  if (name_match(known_names, "Alphonse Gabriel Capone") !== true) {
+  matchSubject
+  //Merge all result arrs with other arrs
+    .zipAll()
+    //Take emissions
+    .subscribe(
+      (result) => {
+        let onlyTrueReducer = (carry, value) => {
+          return value === true || carry === true ? true : false;
+        };
+        let flattenReduce = R.compose(R.reduce(onlyTrueReducer, false), R.flatten);
+        let matchResult = flattenReduce(result);
+        callback(null, matchResult)
+      },
+      (error) => {
+        callback(error, null);
+      },
+      ()=> log('done')
+    );
+}
+
+var known_names = ["Alphonse Gabriel Capone", "Al Capone"];
+
+name_match(known_names, "Alphonse Gabriel Capone", (error, result) => {
+  if (result !== true) {
     console.log('error1');
   }
-  if (name_match(known_names, "Al Capone") !== true) {
+});
+
+name_match(known_names, "Al Capone", (error, result) => {
+  if (result !== true) {
     console.log('error2');
   }
-  if (name_match(known_names, "Alphonse Francis Capone") !== false) {
+});
+
+name_match(known_names, "Alphonse Francis Capone", (error, result) => {
+  if (result !== false) {
     console.log('error3');
   }
+});
 
-  var known_names = ["Alphonse Capone"];
-  if (name_match(known_names, "Alphonse Gabriel Capone") !== true) {
+//CHANGE INPUTS
+known_names = ["Alphonse Capone"];
+
+name_match(known_names, "Alphonse Gabriel Capone", (error, result) => {
+  if (result !== true) {
     console.log('error4');
   }
-  if (name_match(known_names, "Alexander Capone") !== false) {
+});
+
+name_match(known_names, "Alexander Capone", (error, result) => {
+  if (result !== false) {
     console.log('error5');
   }
+});
 
-  var known_names = ["Alphonse Gabriel Capone"];
-  if (name_match(known_names, "Alphonse Capone") !== true) {
+known_names = ["Alphonse Gabriel Capone"];
+name_match(known_names, "Alphonse Capone", (error, result) => {
+  if (result !== true) {
     console.log('error6');
   }
+});
 
-  var known_names = ["Alphonse Gabriel Capone", "Alphonse Francis Capone"];
-  if (name_match(known_names, "Alphonse Gabriel Capone") !== true) {
+var known_names = ["Alphonse Gabriel Capone", "Alphonse Francis Capone"];
+name_match(known_names, "Alphonse Gabriel Capone", (error, result) => {
+  if (result !== true) {
     console.log('error7');
   }
-  if (name_match(known_names, "Alphonse Francis Capone") !== true) {
+});
+
+name_match(known_names, "Alphonse Francis Capone", (error, result) => {
+  if (result !== true) {
     console.log('error8');
   }
-  if (name_match(known_names, "Alphonse Edward Capone") !== false) {
+});
+
+name_match(known_names, "Alphonse Edward Capone", (error, result) => {
+  if (result !== false) {
     console.log('error9');
   }
+});
 
-  var known_names = ["Alphonse Gabriel Capone", "Alphonse F Capone"];
-  if (name_match(known_names, "Alphonse G Capone") !== true) {
+known_names = ["Alphonse Gabriel Capone", "Alphonse F Capone"];
+name_match(known_names, "Alphonse G Capone", (error, result) => {
+  if (result !== true) {
     console.log('error10');
   }
-  if (name_match(known_names, "Alphonse Francis Capone") !== true) {
+});
+
+name_match(known_names, "Alphonse Francis Capone", (error, result) => {
+  if (result !== true) {
     console.log('error11');
   }
-  if (name_match(known_names, "Alphonse E Capone") !== false) {
+});
+
+name_match(known_names, "Alphonse E Capone", (error, result) => {
+  if (result !== false) {
     console.log('error12');
   }
+});
 
-  var known_names = ["Alphonse Gabriel Capone"];
-  if (name_match(known_names, "Gabriel Alphonse Capone") !== true) {
-    console.log('error13');
-  }
-  if (name_match(known_names, "Gabriel Capone") !== true) {
-    console.log('error14');
-  }
-  if (name_match(known_names, "Gabriel A Capone") !== true) {
-    console.log('error15');
-  }
-  if (name_match(known_names, "Capone Francis Alphonse") !== false) {
-    console.log('error16');
-  }
-
-  var known_names = ["Alphonse Capone"];
-  if (name_match(known_names, "Alphonce Capone") !== true) {
-    console.log('error17');
-  }
-  if (name_match(known_names, "Alphonce Capome") !== true) {
-    console.log('error18');
-  }
-  if (name_match(known_names, "Alphons Capon") !== true) {
-    console.log('error19');
-  }
-  if (name_match(known_names, "Alphosne Capone") !== false) {
-    console.log('error20');
-  }
-  if (name_match(known_names, "Alfonse Capone") !== false) {
-    console.log('error21');
-  }
-}
-test();
+// var known_names = ["Alphonse Gabriel Capone"];
+// if (name_match(known_names, "Gabriel Alphonse Capone") !== true) {
+//   console.log('error13');
+// }
+// if (name_match(known_names, "Gabriel Capone") !== true) {
+//   console.log('error14');
+// }
+// if (name_match(known_names, "Gabriel A Capone") !== true) {
+//   console.log('error15');
+// }
+// if (name_match(known_names, "Capone Francis Alphonse") !== false) {
+//   console.log('error16');
+// }
+//
+// var known_names = ["Alphonse Capone"];
+// if (name_match(known_names, "Alphonce Capone") !== true) {
+//   console.log('error17');
+// }
+// if (name_match(known_names, "Alphonce Capome") !== true) {
+//   console.log('error18');
+// }
+// if (name_match(known_names, "Alphons Capon") !== true) {
+//   console.log('error19');
+// }
+// if (name_match(known_names, "Alphosne Capone") !== false) {
+//   console.log('error20');
+// }
+// if (name_match(known_names, "Alfonse Capone") !== false) {
+//   console.log('error21');
+// }
